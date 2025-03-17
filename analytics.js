2f0e8c7b16d28588cc27c546c24cbd2baba99259
miraikumiko <miraikumiko@disroot.org>
2025-03-17 01:56:41 +0100

    Updated

--- analytics.js
+++ analytics.js
@@ -1,22 +1,22 @@
 async function getIpAndCountry() {
     try {
-        const response = await fetch("https://freeipapi.com/api/json");
-        const data = await response.json();
+        const response = await fetch("https://freeipapi.com/api/json")
+        const data = await response.json()
         return {
             ip: data.ipAddress,
             country: data.countryName
-        };
+        }
     } catch (error) {
-        return { ip: "Unknown", country: "Unknown" };
+        return { ip: "Unknown", country: "Unknown" }
     }
 }
 
 
-const url = "https://analiytics.example.com/api/send";
-const referrer = document.referrer;
-const userAgent = navigator.userAgent;
-const pageUrl = window.location.href;
-const clientData = await getIpAndCountry();
+const url = "https://analytics.example.com/api/send"
+const referrer = document.referrer
+const userAgent = navigator.userAgent
+const pageUrl = window.location.href
+const clientData = await getIpAndCountry()
 const data = {
     referrer: referrer,
     user_agent: userAgent,
